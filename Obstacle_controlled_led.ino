1776d93fdd7b43fd4130107ceb83af47e35e543d
Nachiketa Hebbar <54833985+nachi-hebbar@users.noreply.github.com>
2020-04-27 19:50:51 +0530

    Add files via upload

---
+++ Obstacle_controlled_led.ino
@@ -0,0 +1,37 @@
+
+
+
+int ECHO=7;
+int Trig=8;
+int led_vcc=10;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led_vcc,OUTPUT);
+pinMode(ECHO,INPUT);
+pinMode(Trig,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(Trig,LOW);
+delay(5);
+digitalWrite(Trig,HIGH);
+delay(50);
+digitalWrite(Trig,LOW);
+int duration= pulseIn(ECHO,HIGH);
+
+int cm=duration/29/2;
+Serial.println(cm);
+
+if( cm<100  )                                                                                                                                                                                                                                               )
+
+{
+digitalWrite(led_vcc,HIGH);
+}
+else
+{digitalWrite(led_vcc,LOW);
+  }
+
+}
